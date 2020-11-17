b01babf68e0a6db4a764e695c7027638e7adb2ea
Daniel Patricio Ventura <dpvandroid54@gmail.com>
2020-11-16 22:49:52 -0400

    pokeapp without Documentation

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import{ Poke} from '../pokeinterface/poke'
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,40 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+pokeUrl:string;
+poke:Poke={};
+pokemon:string | number;
+url:string="https://pokeapi.co/api/v2/pokemon/";
 
-}
+  constructor(private http:HttpClient) {}
+
+
+
+      getpokemon(url:string):any{
+ 
+     return this.http.get(url);
+ 
+      }
+
+   SearchPokemon(SearchTerm:string | number){
+     this.SearchTerm= encodeURIComponent(this.pokemon).trim();
+     this.pokeUrl=`${this.url}${this.pokemon}`;
+
+
+
+         
+    this.getpokemon(this.pokeUrl)
+        .subscribe(data=>{
+     
+       this.poke.name= data.name;
+       this.poke.weight= data.weight;
+       
+      //  this.poke.img= data.sprites.front_default;
+
+  
+          });
+
+
+      }
+
+   }
---
+++ src/app/pokeinterface/poke.ts
@@ -0,0 +1,7 @@
+export interface Poke {
+
+    name:string;
+    img:string;
+    weight:number;
+    
+}
